ff4f3d8a0fb99b41486f04e3f37478d27244e769
JakubPatera <patera.jakub686@gmail.com>
2023-09-23 15:04:28 +0200

    uprava video pro mobilní přehrávání

--- src/BoatVideo.tsx
+++ src/BoatVideo.tsx
@@ -2,7 +2,7 @@ import Video from '../public/assets/boat-video.mp4'
 
 function BoatVideo() {
   return (
-   <video  className="h-100vh w-full object-cover animate-clip-from-top-animation" autoPlay muted loop>
+   <video  className="h-100vh w-full object-cover animate-clip-from-top-animation" autoPlay muted loop playsInline >
       <source src={Video} type="video/mp4" />
    </video>
   )
