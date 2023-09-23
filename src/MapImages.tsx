27a79454478262bf4974ecad2a26aa0ce7c18fee
JakubPatera <patera.jakub686@gmail.com>
2023-09-23 14:46:40 +0200

    uprava map img

--- src/MapImages.tsx
+++ src/MapImages.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { animationStart } from "./utils/animations";
+import LeftMap from "../public/assets/map_1.png";
+import RightMap from "../public/assets/map_2.png";
 function MapImages() {
   return (
     <div className="absolute w-full flex justify-between transform -translate-y-200px -z-1 overflow-hidden">
@@ -7,13 +9,13 @@ function MapImages() {
         initial={{ x: -100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ delay: animationStart + 1, duration: 1 }}
-        src="../public/assets/map_1.png"
+        src={LeftMap}
       />
       <motion.img
         initial={{ x: 100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ delay: animationStart + 1.5, duration: 1 }}
-        src="../public/assets/map_2.png"
+        src={RightMap}
       />
     </div>
   );
